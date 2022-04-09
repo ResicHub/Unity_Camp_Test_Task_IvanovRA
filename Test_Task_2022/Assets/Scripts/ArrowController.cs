58dc7259ba5bfd57fa2983d647734618343d46cf
Ruslan <resic.resic@yandex.ru>
2022-04-09 15:34:58 +0700

    Completing an ArrowController.
    Adding some methods for script to work correctly.

--- Test_Task_2022/Assets/Scripts/ArrowController.cs
+++ Test_Task_2022/Assets/Scripts/ArrowController.cs
@@ -5,22 +5,74 @@ using UnityEngine;
 /// </summary>
 public class ArrowController : MonoBehaviour
 {
+    private Transform myTransform;
+
     [SerializeField]
     private LineRenderer lineRenderer;
 
-    private Transform myTransform;
+    private bool isMoving = false;
+
+    private int nextPointIndex;
+
+    private Vector3 nextPoint;
 
     private void Awake()
     {
         myTransform = GetComponent<Transform>();
-        myTransform.position = lineRenderer.GetPosition(0);
-        RotateTo(lineRenderer.GetPosition(1));
+        GetReady();
     }
 
-    private void RotateTo(Vector3 target)
+    private void Update()
     {
-        Vector3 direction = target - myTransform.position;
+        if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
+        {
+            GetReady();
+            isMoving = true;
+        }
+        if (isMoving)
+        {
+            Move();
+        }
+    }
+
+    private void GetReady()
+    {
+        myTransform.position = lineRenderer.GetPosition(0);
+        nextPointIndex = 0;
+        ResetNextPoint();
+        RotateToNextPoint();
+    }
+
+    private void RotateToNextPoint()
+    {
+        Vector3 direction = nextPoint - myTransform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    private void ResetNextPoint()
+    {
+        nextPointIndex++;
+        if (nextPointIndex < lineRenderer.positionCount)
+        {
+            nextPoint = lineRenderer.GetPosition(nextPointIndex);
+            RotateToNextPoint();
+        }
+        else
+        {
+            isMoving = false;
+        }
+    }
+
+    private void Move()
+    {
+        if ((nextPoint - myTransform.position).magnitude > 0.01f)
+        {
+            myTransform.position = myTransform.position + myTransform.right * Time.deltaTime;
+        }
+        else
+        {
+            ResetNextPoint();
+        }
     }
 }
